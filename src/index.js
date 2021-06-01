35a5b25b32101def77bfc28df3c6800af3b4742f
akerpelm <akerpelm@u.rochester.edu>
2021-06-01 11:11:40 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles/index.scss";
+// import canvasExample from "./scripts/canvas";
+// import Square from "./scripts/square";
+// import { DOMExample } from "./scripts/DOMExample";
+
+
+document.querySelector(".test").innerHTML = `
+<h2> Hi </h2>
+<p> test </p>
+`
