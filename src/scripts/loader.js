e2bd745a2fca716764e858a61044fbccf0ef559b
akerpelm <akerpelm@u.rochester.edu>
2021-06-26 14:35:48 -0400

    check spelling

---
+++ src/scripts/loader.js
@@ -0,0 +1,10 @@
+// let load;
+// function runLoader() {
+//   load = setTimeout(showPage, 3000);
+// }
+
+// function showPage() {
+//   console.log("loader");
+//   document.getElementsById("loader").style.display = "none";
+//   document.getElementsById("tab-container").style.display = "block";
+// }
